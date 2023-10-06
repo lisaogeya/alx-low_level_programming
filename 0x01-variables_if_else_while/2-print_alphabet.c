582b9bfb47cdb90332087eefb97d14e24aa94914
lisaogeya <lisaogeya.l@gmail.com>
2023-10-06 14:58:05 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:I sometimes suffer from insomnia.
+ *
+ * Return:0
+*/
+int main(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
