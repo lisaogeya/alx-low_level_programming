96fa523160276efa72661f46a16d9d0c0ba436b2
lisaogeya <lisaogeya.l@gmail.com>
2023-10-08 12:09:24 -0700

    print numbrzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:Numberz
+ *
+ * Return:0
+*/
+
+int main(void)
+{
+putchar(48);
+putchar('\n');
+
+putchar(49);
+putchar('\n');
+
+putchar(50);
+putchar('\n');
+
+putchar(51);
+putchar('\n');
+
+putchar(52);
+putchar('\n');
+
+putchar(53);
+putchar('\n');
+
+putchar(54);
+putchar('\n');
+
+putchar(55);
+putchar('\n');
+
+putchar(56);
+putchar('\n');
+
+putchar(57);
+putchar('\n');
+
+return (0);
+}
