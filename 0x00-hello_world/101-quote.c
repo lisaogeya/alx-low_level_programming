5121362954d44afe9c1bee8271cc9aaecd527ae0
lisaogeya <lisaogeya.l@gmail.com>
2023-10-09 09:31:32 -0700

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
-write(2, message, 59);
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
 
 return (1);
 }
