72e05ac08d07c69f6205e8e14f753240e45295fa
lisaogeya <lisaogeya.l@gmail.com>
2023-10-09 13:51:07 -0700

    print comb4s

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main-entry point of program
+ *
+ * Description:Do what you do better
+ *
+ * Return:0
+*/
+
+int main(void)
+{
+int dig1, dig2, dig3;
+for (dig1 = 0; dig1 <= 7; dig1++)
+{
+for (dig2 = dig1 + 1; dig2 <= 8; dig2++)
+{
+for (dig3 = dig2 + 1; dig3 <= 9; dig3++)
+{
+putchar(dig1 + '0');
+putchar(dig2 + '0');
+putchar(dig3 + '0');
+
+if (dig1 != 7 || dig2 != 8 || dig3 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
