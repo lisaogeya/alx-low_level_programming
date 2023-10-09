be1389a95856d0ba60a3568d5cc0ada8ee712f87
lisaogeya <lisaogeya.l@gmail.com>
2023-10-09 12:47:38 -0700

    possible single digit combos

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Description:Patience, persistence and perspiration
+ *
+ * Return:0
+*/
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+if (num != 9)
+{
+putchar(',');
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
+
