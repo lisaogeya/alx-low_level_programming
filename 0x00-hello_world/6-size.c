c54a4e0eb505f17edf5e63275d8bc1fcc134afcd
lisaogeya <lisaogeya.l@gmail.com>
2023-10-09 08:37:31 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,19 @@
 */
 int main(void)
 {
+#ifdef _ _LP64_ _
 printf("Size of char : %lu byte(s)\n", sizeof(char));
 printf("Size of int : %lu byte(s)\n", sizeof(int));
 printf("Size of long int : %lu byte(s)\n", sizeof(long int));
 printf("Size of long long int : %lu byte(s)\n", sizeof(long long int));
 printf("Size of float : %lu byte(s)\n", sizeof(float));
+#else
+printf("Size of char : %lu byte(s)\n", sizeof(char));
+printf("Size of int : %lu byte(s)\n", sizeof(int));
+printf("Size of long int : %lu byte(s)\n", sizeof(long int));
+printf("Size of long long int : %lu byte(s)\n", sizeof(long long int));
+printf("Size of float : %lu byte(s)\n", sizeof(float));
+#endif
 
 return (0);
 }
