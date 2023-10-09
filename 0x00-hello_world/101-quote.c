db6da70097527f2f983846babf9a5fe45fdca970
lisaogeya <lisaogeya.l@gmail.com>
2023-10-09 09:20:15 -0700

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main-entry point of program
+ *
+ * Description:UNIX is basically a simple operating system
+ *Return:0
+*/
+
+int main(void)
+{
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59);
+
+return (1);
+}
