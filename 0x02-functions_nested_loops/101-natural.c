fe890bc96b367f5e39efa78e7f2a9e1482011ab3
lisaogeya <lisaogeya.l@gmail.com>
2023-10-12 08:17:27 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Lists all the natural numbers below 1024
+ * Return: 0
+ */
+int main(void)
+{
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
+printf("%d\n", sum);
+
+return (0);
+}
+
