ee80c5419155b420d81f4e8c0d0497e0b63dfb60
lisaogeya <lisaogeya.l@gmail.com>
2023-10-12 06:56:25 -0700

    print from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_to_98- prints all natural numbers to 98
+ * @n:number to begin counting at
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+printf("%d", n);
+_putchar(',');
+_putchar(' ');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,6 +16,7 @@ int print_last_digit(int);
 int add(int, int);
 void times_table(void);
 void jack_bauer(void);
+void print_to_98(int n);
 
 #endif
 
