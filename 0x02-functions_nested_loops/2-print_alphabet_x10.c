4a3e224814418273293f2b0b0f8cae0e252ecc3f
lisaogeya <lisaogeya.l@gmail.com>
2023-10-12 00:56:17 -0700

    10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** print_alphabet_x10-entry point of the program
+ * Return :void
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+char alph = 'a';
+while (alph <= 'z')
+{
+_putchar(alph);
+alph++;
+}
+_putchar('\n');
+}
+}
+
