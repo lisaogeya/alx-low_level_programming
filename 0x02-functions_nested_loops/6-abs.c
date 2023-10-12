81d2178767454a76452dbac0da2a2176ad3fd15e
lisaogeya <lisaogeya.l@gmail.com>
2023-10-12 02:57:09 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs- absolute value of integer
+ *@r:parameters to be checked
+ * Return:0
+ */
+int _abs(int r)
+{
+if (r >= 0)
+{
+_putchar(r);
+}
+else
+{
+_putchar(r *= -1);
+}
+return (0);
+}
