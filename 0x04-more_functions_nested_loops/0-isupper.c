f20f102a1232874c49b0c9d469c90f0ab711c7af
lisaogeya <lisaogeya.l@gmail.com>
2023-10-12 12:25:36 -0700

    isuppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper- Checks uppercase letters
+ *@c: Parameters to be checked
+ *
+ * Return:1 if true, otherwise 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
