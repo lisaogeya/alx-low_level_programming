1fc5b21a507b9f1b2b3153122f54093169005f17
lisaogeya <lisaogeya.l@gmail.com>
2023-10-12 01:51:01 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha- entry point of the program
+ *@c: check for parameters
+ * Return:1 if true
+ * Returns: 0 if false
+ */
+int _isalpha(int c)
+{
+int c = 'a';
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
