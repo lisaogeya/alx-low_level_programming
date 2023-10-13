c67424b51ab458964da5587b21ee45468402e0c5
lisaogeya <lisaogeya.l@gmail.com>
2023-10-13 07:14:01 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main-entry point of program
+ * Return:0
+ */
+int main(void)
+{
+int num;
+for (num = 1; num <= 100; num++)
+{
+if ((num % 3) == 0)
+printf("Fizz");
+else if
+((num % 5) == 0)
+printf("Buzz");
+else if
+((num % 3) == 0 && (num % 5) == 0)
+printf("FizzBuzz");
+else
+printf("%d", num);
+if (num == 100)
+continue;
+printf(" ");
+}
+printf("\n");
+return (0);
+}
