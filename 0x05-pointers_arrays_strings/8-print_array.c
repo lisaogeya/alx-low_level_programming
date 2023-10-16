e63361a96b558499cbbd8061bf2a4a12abe45ca5
lisaogeya <lisaogeya.l@gmail.com>
2023-10-16 13:47:25 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array-prints arrays
+ * @a:array
+ * @n:number of elements in array
+ */
+void print_array(int *a, int n)
+{
+int index;
+for (index = o; index < n; index++)
+{
+printf("%d", a[index]);
+if
+(index == n - 1);
+continue;
+printf(", ");
+}
+printf("\n");
+}
