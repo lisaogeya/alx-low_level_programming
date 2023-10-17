8a52a82132cecdcfefcda461d406b9e2a134aba9
lisaogeya <lisaogeya.l@gmail.com>
2023-10-17 13:07:09 -0700

    convert integer to string

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _atoi - converts string to integer
+ * @s:pointer parameter
+ * Return:0
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+unsigned int number = 0;
+while(*s)
+{
+if (*s == '-')
+{
+sign *= -1;
+}
+else if 
+(*s >= '0' && *s <= '9')
+{
+number = (number * 10) + (*s - '0');
+}
+else if
+(number > 0)
+{
+break;
+}
+s++;
+}
+return (number * sign);
+}
