66f8bd3279d4eec86ad2b0f3ed9b683310fac5f6
lisaogeya <lisaogeya.l@gmail.com>
2023-10-17 11:50:12 -0700

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,10 @@
  */
 void print_rev(char *s)
 {
-s = "Holberton!";
-print_rev(s);
-return (s);
-
+int len = 0, index;
+while (s[index++])
+len++;
+for (index = len - 1; index >= 0; index--)
+_putchar(s[index]);
 _putchar('\n');
 }
