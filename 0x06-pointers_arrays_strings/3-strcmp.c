e409835df9e41b06d1e229eef2bba1379c7d7c77
lisaogeya <lisaogeya.l@gmail.com>
2023-10-19 15:17:45 -0700

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,5 +7,25 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-return (_strcmp(s1, s2));
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (*s1 < *s2)
+{
+return (1);
+}
+s1++;
+s2++;
+}
+if (*s1 == '\0' && *s2 == '\0')
+{
+return (0);
+}
+else if (*s1 == '\0')
+{
+return (-1);
+}
+else
+{
+return (1);
+}
 }
