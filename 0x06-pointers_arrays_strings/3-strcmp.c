0832cd2ff0179ee48af06f1afd708c02a1d44607
lisaogeya <lisaogeya.l@gmail.com>
2023-10-19 13:49:50 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcmp-compares two strings in alphabetic order
+ * @s1:string to be ccompared
+ * @s2:string to be compared
+ * Return:strcmp
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (_strcmp(s1, s2));
+}
