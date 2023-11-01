52ed44fce55fa95458e239e0ef3cb612c1332159
lisaogeya <lisaogeya.l@gmail.com>
2023-11-01 09:58:28 -0700

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main-entry point of the program
+ *@argc:arguments parameters
+ *@argv:array of pointers
+ *Return:0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
