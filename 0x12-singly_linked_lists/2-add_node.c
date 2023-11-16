5da7ae36bafa812c381ab9ed71df819d26ed9e45
lisaogeya <lisaogeya.l@gmail.com>
2023-11-16 10:01:28 -0800

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,4 +7,25 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+char *copy;
+int len;
+list_t *l;
+l = malloc(sizeof(list_t));
+if (l == NULL)
+return (NULL);
 
+copy = strdup(str);
+if (copy == NULL)
+{
+free (l);
+return (NULL);
+}
+for (len = 0; str[len];)
+len++;
+l->str = copy;
+l->len = len;
+l->next = *head;
+
+*head = l;
+return (l);
+}
